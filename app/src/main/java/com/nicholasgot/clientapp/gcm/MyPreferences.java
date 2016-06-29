bbc1b9ab9ddd4a2c7ecd8ada1f5f7ba9a103ebad
Tiglas <nicholas.got@gmail.com>
2016-06-29 14:26:35 +0200

    organize client and vehicle app

--- app/src/main/java/com/nicholasgot/clientapp/gcm/MyPreferences.java
+++
@@ -1,8 +0,0 @@
-package com.nicholasgot.clientapp.gcm;
-
-public class MyPreferences {
-
-    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
-    public static final String REGISTRATION_COMPLETE = "registrationComplete";
-
-}
