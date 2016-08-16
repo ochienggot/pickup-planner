8b84be7338d85dd4736f48993d8543d8f62e6aaa
Tiglas <nicholas.got@gmail.com>
2016-08-16 21:30:01 +0200

    Update trip generator

--- trip_generator/trip_generator.py
+++ trip_generator/trip_generator.py
@@ -10,6 +10,7 @@ import itertools
 import ast
 
 from clustering import cluster_requests
+from CpGdiInterface import CpGdiInterface
 
 
 ''' Travel requests by users stored in the form user=>[source, destination, time], where the time field indicates the time constraints for each user
@@ -24,7 +25,7 @@ class DBConnection():
     
     def fetch_rows(self, dest_id):
         self.cur = self.conn.cursor()
-        self.cur.execute("SELECT request_id, source, destination, destination_id FROM REQUESTS WHERE destination_id=%s", (dest_id,));
+        self.cur.execute("SELECT DISTINCT ON (source) request_id, source, destination, destination_id FROM REQUESTS WHERE destination_id=%s", (dest_id,));
         self.rows = list(self.cur.fetchall())
 
     def get_rows(self):
@@ -72,7 +73,7 @@ def request_distance(str_origins, str_destinations):
     '''
     res = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + str_origins
                        +'&destinations=' + str_destinations +
-                       '&language=swKE&key=AIzaSyC2ShjI7RzF-O7qB5eOdRhNbTCtrj7Z5oQ')
+                       '&language=swKE&key=AIzaSyDY3kE9ShEiHTRzl1a7G-V7wgKeG1HpA0k')
 
     costs = {}
     json_str = json.loads(res.text)
@@ -85,11 +86,30 @@ def request_distance(str_origins, str_destinations):
 
     return json_str['rows'][index]['elements'][index]['distance']['value']
 
+def get_cost_distance(origin, destination):
+    """ Return the distance in metres from origin to destination obtained from GDI
+
+    origin -- start latitude/longitude
+    destination -- end latitude/longitude
+    """
+    gdi = CpGdiInterface()
+    src = origin.split(',')
+    dst = destination.split(',')
+    COST_METRIC = "pollution"
+    gdi.get_city_routes(src[1], src[0], dst[1], dst[0], COST_METRIC, 1)
+
+    cost = gdi.get_route_cost()
+    #return gdi.get_route_cost()
+    # print "Cost of route = " + str(cost)
+    if cost is None:
+        return 0
+    return cost
+
 def build_graph(node_list):
-    ''' Builds graph based on the source locations and adds node costs
-    @param node_list the list of nodes and corresponding lat/lat 
-    @return dense graph with costs calculated
-    '''
+    """ Build and return a weigted graph based on the source locations and adds node costs
+
+    node_list -- the list of nodes and corresponding lat/lat 
+    """
     start_depot = ['59.84228', '17.6443627'] # Polacks
     stop_depot = ['59.6497622', '17.9237807'] # Arlanda
     node_list[1] = start_depot
@@ -103,8 +123,11 @@ def build_graph(node_list):
     cur = conn.cursor()
 
     for n in G.nodes():
-        G.node[n]['latitude'] = node_list[n][0]
-        G.node[n]['longitude'] = node_list[n][1]
+        WT_LAT = 'latitude'
+        WT_LNG = 'longitude'
+        G.node[n][WT_LAT] = node_list[n][0]
+        G.node[n][WT_LNG] = node_list[n][1]
+        # TODO: Add exception handling here
         insert_nodes(n, node_list[n][0], node_list[n][1], cur)
 
     for edge in G.edges():
@@ -112,9 +135,10 @@ def build_graph(node_list):
         node2 = edge[1]
         src_lat, src_lon = node_list[node1][0], node_list[node1][1]
         dst_lat, dst_lon = node_list[node2][0], node_list[node2][1]
-        G[node1][node2]['weight'] = edge_cost([src_lat, src_lon], 
+        EDGE_WEIGHT = 'weight'
+        G[node1][node2][EDGE_WEIGHT] = edge_cost([src_lat, src_lon], 
                 [dst_lat, dst_lon])
-        insert_edges(node1, node2, G[node1][node2]['weight'], cur)
+        insert_edges(node1, node2, G[node1][node2][EDGE_WEIGHT], cur)
 
     conn.commit()
     cur.close()
@@ -147,17 +171,16 @@ def edge_cost(src_loc, dst_loc):
     source = str(src_loc[0]) + ',' + str(src_loc[1])
     destination = str(dst_loc[0])  + ',' + str(dst_loc[1])
 
-    return  request_distance(source, destination)
-
-# TODO: GDI database setup for cost queries
-# TODO: design UI - client app to write requests to DB
+    # Migrated from Google API to GDI
+    #return  request_distance(source, destination) 
+    return get_cost_distance(source, destination)
 
 def destinations():
     ''' Get the destination IDs from the database
     '''
     conn = pg.connect(database="ngot", host="127.0.0.1", port="5432")
     cur = conn.cursor()
-    cur.execute("SELECT DISTINCT destination_id from requests")
+    cur.execute("SELECT DISTINCT destination_id FROM requests")
     dest = []
     for row in cur.fetchall()[0]:
         dest.append(row)
@@ -166,23 +189,21 @@ def destinations():
 
     return 100
     #return dest  # TODO: fix list of destinations
-# TODO: better documentation
 
 def nodes(dest_id):
-    '''
-    Construct graph, group or cluster request and return resulting nodes
-    @param dest_id integer uniquely identifying the destination
+    ''' Construct graph, group or cluster request and return resulting nodes
+
+        dest_id -- integer uniquely identifier for each destination
     '''
     src, dst = get_requests_fromDB(dest_id) 
-    G = build_graph(src) # only unique requests considered
+    G = build_graph(src) # TODO: Graph built one-time
     # Cluster nodes from built graph
 
-    f = open('cluster_results.txt', 'r') # clusters
-    #f = open('7_cluster_results.txt', 'r') # random groups
+    f = open('new_cluster_results.txt', 'r') # clusters from graph
+    #f = open('new_group_results.txt', 'r') # random groups
     all_clusters = ast.literal_eval(f.read())
     f.close()
-    #cluster = cluster_requests(True) # TODO: pass cluster_id
-    cluster_id = 9
+    cluster_id = 7
     cluster = [c for c in all_clusters[cluster_id].keys()]
 
     '''
@@ -224,10 +245,10 @@ def neighbors(node):
     return neighbors
 
 def cost(node1, node2):
-    '''
-    Calculates the cost between two nodes in the graph
-    @param node1 integer uniquely identifying node1
-    @param node2 integer uniquely identifying node2
+    ''' Calculate the cost between two nodes in the graph
+
+    node1 -- unique integer for node1
+    node2 -- unique integer for node2
     '''
     conn = pg.connect(database="ngot", host="127.0.0.1", port="5432")
     cur = conn.cursor()
